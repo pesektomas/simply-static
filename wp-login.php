99fc7a5ba0eb3a54911a35946b3beaf1941d636e
Tomas Pesek <pesek@economia.cz>
2018-04-11 14:11:54 +0200

    first generated file

---
+++ wp-login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="cs-CZ">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="cs-CZ">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Přihlásit se &lsaquo; Blogy &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://simply-static.netlify.com/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.5' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-cs-cz">
+		<div id="login">
+		<h1><a href="https://cs.wordpress.org/" title="Používáme WordPress" tabindex="-1">Používáme WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="https://simply-static.netlify.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Uživatelské jméno nebo email<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Heslo<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Pamatovat si mě</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Přihlásit se" />
+		<input type="hidden" name="redirect_to" value="https://simply-static.netlify.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://simply-static.netlify.com/wp-login.php?action=lostpassword">Zapomněli jste heslo?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://simply-static.netlify.com/">&larr; Zpět: Blogy</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
